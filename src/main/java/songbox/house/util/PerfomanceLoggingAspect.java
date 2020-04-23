0e38eb6d4a892977a407d98e148f1e0b0e0269d9
denstden <denstden@gmail.com>
2020-04-23 21:20:34 +0300

    Dev: Perfomance logging

--- src/main/java/songbox/house/util/PerfomanceLoggingAspect.java
+++ src/main/java/songbox/house/util/PerfomanceLoggingAspect.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import static java.lang.System.currentTimeMillis;
@@ -15,17 +14,12 @@ import static songbox.house.util.Constants.PERFORMANCE_MARKER;
 @Slf4j
 public class PerfomanceLoggingAspect {
 
-    @Pointcut("within(@songbox.house.util.Measurable *)")
-    public void measurable() {
-    }
-
-    @Around("measurable()")
+    @Around("annotation(songbox.house.util.Measurable)")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable {
         final long start = currentTimeMillis();
         Object output = pjp.proceed();
         final long end = currentTimeMillis() - start;
-        log.info(PERFORMANCE_MARKER, "{}{} - {}ms", pjp.getSignature().getDeclaringType().getSimpleName(),
-                pjp.getSignature().getName(), end - start);
+        log.info(PERFORMANCE_MARKER, "{} - {}ms", pjp.getSignature().toShortString(), end - start);
         return output;
     }
 }
